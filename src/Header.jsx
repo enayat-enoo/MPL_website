ee41104d4dd7fad83f9499ecb02de2f161fde4e2
Enayat <enayatansari33@gmail.com>
2025-07-29 12:23:23 +0530

    change the folder path of img logo for deployment

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="w-full text-center p-1 rounded bg-white">
-      <img src="./src/assets/mpllogg.png" alt="" className="w-20 h-20 rounded-full mx-auto" />
+      <img src="./public/assets/mpllogg.png" alt="" className="w-20 h-20 rounded-full mx-auto" />
       <h1 className="text-4xl font-bold text-amber-700 mb-2 tracking-wide font-serif">
         Maskedih Premier League
       </h1>
--- src/pages/MaskedihStrikers.jsx
+++ src/pages/MaskedihStrikers.jsx
@@ -21,7 +21,7 @@ const players = [
 function MaskedihStrikers() {
   return (
     <div className="p-4 bg-white">
-       <img src="./src/assets/maskedihstrikers.jpg" alt="" className="h-20 mx-auto rounded-full" />
+       <img src="./public/assets/maskedihstrikers.jpg" alt="" className="h-20 mx-auto rounded-full" />
       <h1 className="text-3xl font-bold text-center text-cyan-500 mb-6 p-2 rounded">
         Maskedih Strikers
       </h1>
